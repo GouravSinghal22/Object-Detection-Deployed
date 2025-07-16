60fdc73601149eac7a85c2b1bd733ab068688958
Gourav Singhal <gouravsinghal223@gmail.com>
2025-07-16 21:43:39 +0530

    Update 2_YOLO_webrtc.py

--- pages/2_YOLO_webrtc.py
+++ pages/2_YOLO_webrtc.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 from streamlit_webrtc import webrtc_streamer
 import av
+import cv2
 from yolo_predictions import YOLO_Pred
 
 # load yolo model
@@ -10,9 +11,15 @@ yolo = YOLO_Pred('./models/best.onnx',
 
 def video_frame_callback(frame):
     img = frame.to_ndarray(format="bgr24")
-    # any operation 
-    #flipped = img[::-1,:,:]
-    pred_img = yolo.predictions(img)
+
+    # Resize to speed up inference
+    img_resized = cv2.resize(img, (416, 416))
+
+    # YOLO prediction
+    pred_img = yolo.predictions(img_resized)
+
+    # Resize back to display size
+    pred_img = cv2.resize(pred_img, (img.shape[1], img.shape[0]))
 
     return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
 
